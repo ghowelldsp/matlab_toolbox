fea5cf3f76d5e87f06848ad857b65cd9c71d4724
george-howell <georgehowelldsp@gmail.com>
2023-01-29 22:07:26 +0000

    created default display setup function

---
+++ src/general/displaySetup.m
@@ -0,0 +1,12 @@
+function [] = displaySetup()
+% DISPLAYSETUP - sets up default display parameters
+%
+% Created By: George Howell
+% Created On: 29-Jan-2023
+
+set(groot,'defaultAxesFontName','Verdana')
+set(groot,'defaultAxesFontSize', 12)
+set(groot,'defaultAxesLineWidth', 1)
+set(groot,'defaultLineLineWidth', 2);
+
+end
